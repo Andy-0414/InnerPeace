d839991c867244f02daa0d56d05428b1a7290448
Andy0414 <pjh8667@naver.com>
2019-08-19 09:35:35 +0900

    UPDATE logo

--- src/components/information/Information.vue
+++ src/components/information/Information.vue
@@ -1,13 +1,17 @@
 <template>
 	<article class="information">
 		<div ref="background" class="information__background"></div>
+        <div ref="logo" class="information__logo">
+            <img src="@/assets/logo.png" alt="">
+        </div>
 	</article>
 </template>
 <script lang="ts">
 import Vue from "vue";
 export default Vue.extend({
 	mounted() {
-		let background: any = this.$refs.background;
+        let background: any = this.$refs.background;
+        let logo :any = this.$refs.logo;
 		for (let i = 0; i < innerWidth/80; i++) {
 			let div: HTMLDivElement = document.createElement("div");
 			background.appendChild(div);
@@ -17,6 +21,7 @@ export default Vue.extend({
                 let value = 200-Math.abs(x.offsetLeft-e.clientX);
 				x.style.height = `${(value < 0 ? 0 : value)}px`;
 			});
+            logo.style.transform = `translate3d(${(e.clientX-innerWidth/2)/20}px,${(e.clientY-innerHeight/2)/20}px,0)`;
         });
         addEventListener("touchmove", e => {
 			[...background.children].forEach(x => {
@@ -34,6 +39,8 @@ export default Vue.extend({
 	height: 100vh;
 
 	background-color: #121319;
+
+    overflow: hidden;
 }
 .information__background {
 	position: absolute;
@@ -47,6 +54,25 @@ export default Vue.extend({
 	display: flex;
 	justify-content: center;
 	align-items: flex-end;
+}
+.information__logo{
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    color: white;
+    
+    transition: 2s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+}
+.information__logo img{
+    width: 20%;
 }
 .information__background div {
 	position: relative;
